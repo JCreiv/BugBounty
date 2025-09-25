4512bc85619d2bf8ccf1e8032fdc1062b63b8779
Jcreiv <blasllamagris@gmail.com>
2025-09-25 02:40:19 +0200

    Añadir filtro de gau y cambiar variable

--- recon.sh
+++ recon.sh
@@ -8,6 +8,7 @@ set -euo pipefail
 #  CONFIGURACIÓN (edita si hace falta)
 ###############
 DIR="results"   # base para resultados
+DOMAIN="$1"   # dominio objetivo
 # formato timestamp sin segundos: YYYYMMDD
 TIME=$(date "+%Y%m%d")
 ###############
@@ -19,10 +20,9 @@ Uso:
   $0 dominio.tld
 
 Genera:
-  ${DIR}/<dominio>/<YYYYMMDD>/raw/
-  ${DIR}/<dominio>/<YYYYMMDD>/clean/
+  ${DIR}/<dominio>/<YYYYMMDD>/domains/
+  ${DIR}/<dominio>/<YYYYMMDD>/urls/
 
-El script espera que 'findomain' esté en PATH.
 EOF
   exit 1
 }
@@ -32,20 +32,20 @@ if [ $# -eq 0 ]; then
   usage
 fi
 
-# comprobar findomain
-if ! command -v findomain >/dev/null 2>&1; then
-  echo "ERROR: 'findomain' no encontrado en PATH. Instálalo e inténtalo de nuevo." >&2
-  exit 2
-fi
-
 # Crear directorios
 
-TARGET_DIR="$DIR/$1/$TIME"
+TARGET_DIR="$DIR/$DOMAIN/$TIME"
 
 mkdir -p $TARGET_DIR/domains/raw
 mkdir -p $TARGET_DIR/domains/clean
 mkdir -p $TARGET_DIR/urls/raw
 mkdir -p $TARGET_DIR/urls/clean
+
+
+
+# -----------------------------------
+# Recolección de dominios/subdominios
+# -----------------------------------
 
 # -----------------------------------
 # Ejecutar findomain
@@ -55,7 +55,7 @@ if [ -f $TARGET_DIR/domains/raw/findomain_raw.txt ]; then
 fi
 
 
-findomain -t $1 -r -u $TARGET_DIR/domains/raw/findomain_raw.txt
+findomain -t $DOMAIN -r -u $TARGET_DIR/domains/raw/findomain_raw.txt
 
 sort -u $TARGET_DIR/domains/raw/findomain_raw.txt > $TARGET_DIR/domains/clean/findomain_clean.txt
 
@@ -67,7 +67,7 @@ if [ -f $TARGET_DIR/domains/raw/sublist3r_raw.txt ]; then
     rm $TARGET_DIR/domains/raw/sublist3r_raw.txt
 fi
 
-sublist3r -d $1 -o $TARGET_DIR/domains/raw/sublist3r_raw.txt
+sublist3r -d $DOMAIN -o $TARGET_DIR/domains/raw/sublist3r_raw.txt
 
 sort -u $TARGET_DIR/domains/raw/sublist3r_raw.txt > $TARGET_DIR/domains/clean/sublist3r_clean.txt
 
@@ -79,7 +79,7 @@ if [ -f $TARGET_DIR/domains/raw/subfinder_raw.txt ]; then
     rm $TARGET_DIR/domains/raw/subfinder_raw.txt
 fi
 
-subfinder -d $1 -o $TARGET_DIR/domains/raw/subfinder_raw.txt
+subfinder -d $DOMAIN -o $TARGET_DIR/domains/raw/subfinder_raw.txt
 
 sort -u $TARGET_DIR/domains/raw/subfinder_raw.txt > $TARGET_DIR/domains/clean/subfinder_clean.txt
 
@@ -90,7 +90,7 @@ if [ -f $TARGET_DIR/domains/raw/cero_raw.txt ]; then
     rm $TARGET_DIR/domains/raw/cero_raw.txt
 fi
 
-cero -d $1 > $TARGET_DIR/domains/raw/cero_raw.txt
+cero -d $DOMAIN > $TARGET_DIR/domains/raw/cero_raw.txt
 
 sort -u $TARGET_DIR/domains/raw/cero_raw.txt > $TARGET_DIR/domains/clean/cero_clean.txt
 
@@ -101,21 +101,48 @@ cat $TARGET_DIR/domains/clean/*_clean.txt | sort -u > $TARGET_DIR/domains/${1}_d
 
 dnsx -l $TARGET_DIR/domains/${1}_domains_all_clean.txt -o $TARGET_DIR/domains/${1}_domains_all_dnsx.txt
 
-# -----------------------------------
-# Ejecutar gau
 
-if [ -f $TARGET_DIR/urls/raw/gau_raw.txt ]; then
-  rm $TARGET_DIR/urls/raw/gau_raw.txt
+if [ -f $TARGET_DIR/domains/${1}_domains_all_dnsx.txt ]; then
+  rm $TARGET_DIR/domains/${1}_domains_all_dnsx.txt
 fi
+
+if [ -f $TARGET_DIR/domains/${1}_domains_all_clean.txt ]; then
+  rm $TARGET_DIR/domains/${1}_domains_all_clean.txt
+fi
+
+# -----------------------------------
+# Crawling y filtrado de URLs
+# -----------------------------------
+
+# -----------------------------------
+# Ejecutar gau con httpx
 
 gau --threads 50 \
     --blacklist "jpg,jpeg,png,gif,svg,css,js,ico,pdf,zip,tar,gz" \
-    $1 \
-    --o $TARGET_DIR/urls/raw/gau_raw.txt $(cat $TARGET_DIR/domains/${1}_domains_all_clean.txt)
+    $DOMAIN \
+    --o $TARGET_DIR/urls/raw/gau_raw.txt $(cat $TARGET_DIR/domains/${1}_domains_all_dnsx.txt)
 
 httpx -l $TARGET_DIR/urls/raw/gau_raw.txt -t 100 -sc \
       -mc 200,201,204,301,302,307,401,403,405,500 \
       -o $TARGET_DIR/urls/raw/gau_httpx_raw.txt
 
+if [ -f $TARGET_DIR/urls/raw/gau_raw.txt ]; then
+  rm $TARGET_DIR/urls/raw/gau_raw.txt
+fi
 
 
+sort -u "$TARGET_DIR/urls/raw/gau_httpx_raw.txt" | while read -r line; do
+    code=$(echo "$line" | sed -r 's/\x1B\[[0-9;]*[mK]//g' | awk -F'[][]' '{print $2}')
+    url=$(echo "$line" | awk '{print $1}')
+
+    case $code in
+        200) echo "$url" > "$TARGET_DIR/urls/clean/url_200.txt";;
+        302) echo "$url" > "$TARGET_DIR/urls/clean/url_302.txt";;
+        403) echo "$url" > "$TARGET_DIR/urls/clean/url_403.txt";;
+    esac
+done
+
+# -----------------------------------
+# Ejecutar katana
+
+katana 
