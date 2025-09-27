5279a835ca5abf36bd525f6ea2319f859420a773
Jcreiv <blasllamagris@gmail.com>
2025-09-27 15:03:18 +0200

    Añadir katana

--- recon.sh
+++ recon.sh
@@ -117,32 +117,38 @@ fi
 # -----------------------------------
 # Ejecutar gau con httpx
 
+# Ejecutar gau
 gau --threads 50 \
     --blacklist "jpg,jpeg,png,gif,svg,css,js,ico,pdf,zip,tar,gz" \
     $DOMAIN \
     --o $TARGET_DIR/urls/raw/gau_raw.txt $(cat $TARGET_DIR/domains/${1}_domains_all_dnsx.txt)
 
-httpx -l $TARGET_DIR/urls/raw/gau_raw.txt -t 100 -sc \
+# Ejecutar katana
+katana -list $TARGET_DIR/domains/${1}_domains_all_dnsx.txt \
+    -ef jpg,jpeg,png,gif,svg,css,js,ico,pdf,zip,tar,gz \
+    -c 50 \
+    -o $TARGET_DIR/urls/raw/katana_raw.txt
+
+# Unir y limpiar duplicados
+cat $TARGET_DIR/urls/raw/gau_raw.txt $TARGET_DIR/urls/raw/katana_raw.txt | sort -u > $TARGET_DIR/urls/raw/urls_raw.txt
+
+# Pasar por httpx
+httpx -l $TARGET_DIR/urls/raw/urls_raw.txt -t 100 -sc \  
       -mc 200,201,204,301,302,307,401,403,405,500 \
-      -o $TARGET_DIR/urls/raw/gau_httpx_raw.txt
+      -o $TARGET_DIR/urls/raw/httpx_raw.txt
 
-if [ -f $TARGET_DIR/urls/raw/gau_raw.txt ]; then
-  rm $TARGET_DIR/urls/raw/gau_raw.txt
-fi
+# Eliminar raws individuales para dejar solo el unificado
+rm -f $TARGET_DIR/urls/raw/gau_raw.txt $TARGET_DIR/urls/raw/katana_raw.txt
 
-
-sort -u "$TARGET_DIR/urls/raw/gau_httpx_raw.txt" | while read -r line; do
+# Clasificar por códigos de estado
+sort -u "$TARGET_DIR/urls/raw/httpx_raw.txt" | while read -r line; do
     code=$(echo "$line" | sed -r 's/\x1B\[[0-9;]*[mK]//g' | awk -F'[][]' '{print $2}')
     url=$(echo "$line" | awk '{print $1}')
 
     case $code in
-        200) echo "$url" > "$TARGET_DIR/urls/clean/url_200.txt";;
-        302) echo "$url" > "$TARGET_DIR/urls/clean/url_302.txt";;
-        403) echo "$url" > "$TARGET_DIR/urls/clean/url_403.txt";;
+        200) echo "$url" >> "$TARGET_DIR/urls/clean/url_200.txt";;
+        302) echo "$url" >> "$TARGET_DIR/urls/clean/url_302.txt";;
+        403) echo "$url" >> "$TARGET_DIR/urls/clean/url_403.txt";;
     esac
 done
 
-# -----------------------------------
-# Ejecutar katana
-
-katana 
